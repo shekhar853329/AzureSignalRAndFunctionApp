ded23a57c92a6a30d1e1c8a0ad885c8fbad38800
Shekhar Kumar <shekhar.kumar@siemens.com>
2024-01-30 14:44:26 +0530

    testing for deployment

--- SignalRAndFunctionApp/TCM.Web.Api/TCM.Azure.Function/TestFunction.cs
+++ SignalRAndFunctionApp/TCM.Web.Api/TCM.Azure.Function/TestFunction.cs
@@ -26,8 +26,8 @@ namespace TCM.Azure.Function
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.Testing"
+                : $"Hello, {name}. This HTTP triggered function executed successfully.Testing";
 
             return new OkObjectResult(responseMessage);
         }
